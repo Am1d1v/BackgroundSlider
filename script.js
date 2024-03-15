9ce583a0c7c6e3d13652ab9193cc9b3a5bd6827b
Am1d1v <dimadiman1adima@gmail.com>
2024-03-15 13:58:16 +0300

    Swipe images to left/right

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 
 
 
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.querySelector('#left');
+const rightBtn = document.querySelector('#right');
+
+let activeSlide = 0;
+
+function setBGToBody(){
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+// Set initial background
+setBGToBody();
+
+// Swipe to right
+rightBtn.addEventListener('click', () => {
+
+    if(activeSlide >= slides.length - 1){
+        activeSlide = 0;
+    }
+
+    activeSlide++;
+    console.log(activeSlide);
+    setBGToBody();
+});
+
+// Swipe to left
+leftBtn.addEventListener('click', () => {
+    if(activeSlide <= 0){
+        activeSlide = slides.length - 1;
+    }
+
+    activeSlide--;
+    console.log(activeSlide);
+    setBGToBody();
+
+});
+
